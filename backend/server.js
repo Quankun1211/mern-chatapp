545b6e28d06aa21a8accef65db82f30211ed8a40
Quankun1211 <quan7ammm@gmail.com>
2024-04-20 23:43:40 +0700

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import connect from "./db/connectDB.js"
 import express from "express"
 import dotenv from "dotenv"
@@ -9,6 +10,8 @@ import { app, server } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json())
@@ -18,6 +21,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/user', userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
   connect()
